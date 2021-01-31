5814eadc7c1ead8db0db11facb89d41cacea0dc8
Skip Montanaro <skip.montanaro@gmail.com>
2021-01-31 14:07:36 -0600

    bump coverage a bit by executing controlled 'read' command

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,8 +2,14 @@
 
 # Run tests. Output is compared with expected.out
 
+# If TEST_CRED is set, server, user and password are grepped out of it
+# and added to test.cfg. In that case, it is expected that the
+# referenced IMAP account will have a test-polly folder/label. This is
+# only used to exercise the read loop and increase coverage, so it's
+# not strctly necessary.
+
 export TMPDIR=$(mktemp -d)
-trap "rm -rf ${TMPDIR}" EXIT
+# trap "rm -rf ${TMPDIR}" EXIT
 
 if [ "x$1" = "x--generate" ] ; then
     OUT=/dev/stdout
@@ -15,7 +21,6 @@ fi
 
 coverage erase
 rm -rf htmlcov
-rm -f /tmp/trash
 for f in $(ls tests/cfgs/test-*.cfg) ; do
     echo "* $f"
     coverage run --append src/polly.py -g 5 -c ${f}
@@ -24,43 +29,41 @@ done > ${OUT}
 
 if [ "x$GENONLY" = "x0" ] ; then
     echo '[Polly]
-folder = polly
+verbose = trace
+folder = test-polly
 threshold = 0.25
 nwords = 8192
-verbose = INFO
 punctuation = False
 digits = False
 upper = False
 maxchars = 6
 length = 4
 logfile = /dev/null
-picklefile = '${TMPDIR}/polly.cfg'
+picklefile = '${TMPDIR}'/polly.pkl
+unittests = True
 ' > ${TMPDIR}/test.cfg
 
-    echo flippant > /tmp/extra.words
+    if [ "x$TEST_CRED" != "x" ] ; then
+        egrep '^ *(server|user|password) *=' $TEST_CRED >> ${TMPDIR}/test.cfg
+    fi
+    echo flippant > ${TMPDIR}/extra.words
     echo 'bad djm efore etty fter ginal ilt ong tle
-    add /tmp/extra.words 1
+    add '${TMPDIR}'/extra.words 1
     good polly.good
     dict american-english-large.txt
-    option verbose debug
     option logfile /tmp/trash
     option digits false
     option length 7
     option editing-mode vi
-    option bogus true
-    option
-    stat
-    password 1
-    ' | coverage run --append src/polly.py \
-                 -c ${TMPDIR}/test.cfg > /dev/null
+    read
+    sleep 5
+    sleep 5
+    sleep 5
+    sleep 5
+    sleep 5
+    ' | coverage run --concurrency=thread --append src/polly.py -n \
+                 -c ${TMPDIR}/test.cfg
 
-    echo 'read
-    sleep 15
-    exit
-    ' | coverage run --concurrency=thread --append src/polly.py \
-                 -c ${TMPDIR}/test.cfg > /dev/null
     coverage html
-    trap "rm -f /tmp/extra.words" EXIT
-
     diff -u tests/output/expected.out ${OUT}
 fi
